fa8ddc378cbccd98b2c732b08abc4c00f8b169c4
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-05-26 08:03:20 -0300

    up

---
+++ src/br/vectorfor1/VectorFor1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package br.vectorfor1;
+
+/**
+ *
+ * @author Marce
+ */
+public class VectorFor1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n[] = {1,8,6,7,4};
+        System.out.println("Total de casas do vetor: " + n.length);
+        for (int cc=0 ; cc <=4; cc++){
+            System.out.println("Na prosicao "+ cc + " temos o valor " + n[cc]);
+        }
+    }
+    
+}
